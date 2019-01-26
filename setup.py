bd604fcb2472e26899ea72702666b0025eab713a
Meng Pan <pan5431333@hotmail.com>
2019-01-26 12:02:12 +0800

    update version number to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyoptree',
-    version="0.1.1",
+    version="0.1.2",
     description=(
         "Python Implementation of Bertsimas's paper Optimal Classification Trees."
     ),
