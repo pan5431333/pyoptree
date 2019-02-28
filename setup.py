5415ba40d5262a2e82c34bc6888facf5a92066c6
Meng Pan <pan5431333@hotmail.com>
2019-02-28 20:44:58 +0800

    update version to 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyoptree',
-    version="0.1.3",
+    version="0.1.4",
     description=(
         "Python Implementation of Bertsimas's paper Optimal Classification Trees."
     ),
@@ -21,7 +21,8 @@ setup(
     install_requires=[
         'numpy>=1.14.5',
         'pandas>=0.23.1',
-        'pyomo>=5.5.0'
+        'pyomo>=5.5.0',
+        'scikit-learn>=0.20.0'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
